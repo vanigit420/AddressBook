69bde3a5ad9be3e1b0166fe6736359b39f247a6f
shivani korwan <shivanikorwan63232@gmail.com>
2022-05-31 12:47:19 +0530

    enter contact number

---
+++ AddressBook/AddressBook/Contact.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipcode { get; set; }
+        public string email { get; set; }
+        public string phoneNumber { get; set; }
+
+    }
+
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace AddressBook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            string address;
+            string city;
+            string state;
+            string zipcode;
+            string email;
+            string phoneNumber;
+
+            Console.WriteLine("Enter the FirstName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the lastName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the address : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the city : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the state : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the zipcode : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the email : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the phoneNumber : ");
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
